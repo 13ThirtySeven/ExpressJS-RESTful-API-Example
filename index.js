325926e283a85868421e379a4423426abca8b35b
Amro Soliman <99121380+13ThirtySeven@users.noreply.github.com>
2022-02-08 18:17:08 +0000

    separate the routing logic

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 const express = require('express');
+
+// require flights route
+const flights = require('./routes/flights');
+
 const app = express();
 
-
-
-// Hard coded Flights Object just for the
-//purpose of demonstration 
-
-const flights = [
-
-    { id: 1, name: "Luftahanza", flightNumber: "185B"  },
-    { id: 2, name: "RyanAir", flightNumber: "163CB"  },
-    { id: 3, name: "WizzAir", flightNumber: "205NR"  },
-    { id: 4, name: "Qatar AirWays", flightNumber: "60CD"  },
-    { id: 5, name: "Egyptian Air", flightNumber: "376B"  },
-    { id: 6, name: "SAS", flightNumber: "453WD"  }
-   
-   ];
-
-
-// route to get all flights, Endpoint is => localhost:3000/flights
-app.get('/flights', (req,res) => {
-
-       res.send(flights);
-    
-   });
+// using flights route with the app instance
+app.use('/flights', flights);
 
 
 const port = process.env.port || 3000;
